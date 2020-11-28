baaa627ff71b7f4022bcdbd8fe58f7268fcc20df
Ganesan Chinnathambi <ganesancp@gmail.com>
2020-11-28 08:18:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# vutils_pkg
+SV & UVM verification utilities that includes:
+  - convenience macros
+  - useful typedefs
+  - helper methods
---
+++ vutils_types.svh
@@ -0,0 +1,12 @@
+
+// boolean type
+typedef enum bit {
+  FALSE = 1'b0,
+  TRUE  = 1'b1
+} bool_e;
+
+// unsigned 2-state int types
+typedef bit [  7:0] uchar_t;
+typedef bit [ 15:0] uint16_t;
+typedef bit [ 31:0] uint32_t;
+typedef bit [ 63:0] uint64_t;
